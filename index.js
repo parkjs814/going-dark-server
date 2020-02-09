4b974f3dad49b2c8f50990cb0517249bb782748a
Jinseo Park <jason.park@gatech.edu>
2020-02-09 00:14:53 -0500

    d

---
+++ index.js
@@ -0,0 +1,53 @@
+const app = require('http').createServer();
+const io = require('socket.io')(app);
+
+app.listen(8080);
+
+const ROOM_LOBBY = '1@bby';
+const games = [];
+
+io.on('connection', function (socket) {
+  socket.join(ROOM_LOBBY);
+  socket.on('createGame', (gameId, game) => {
+    socket.leave(ROOM_LOBBY);
+    socket.join(gameId);
+    games.push(game);
+    socket.emit('joinGame', game);
+  });
+  socket.on('joinGame', gameId => {
+    const game = games.find(game => game.id === gameId);
+    if (!game) return;
+    socket.leave(ROOM_LOBBY);
+    socket.join(gameId);
+    socket.emit('joinGame', game);
+  });
+  socket.on('leaveGame', gameId => {
+    socket.leave(gameId);
+    socket.join(ROOM_LOBBY);
+  });
+  socket.on('updateGame', updatedGame => {
+    const game = games.find(game => game.id === updatedGame.id);
+    if (!game) return;
+    Object.assign(game, updatedGame);
+    socket.broadcast.to(game.id).emit('updateGame', updatedGame);
+  });
+  socket.on('updateMap', (gameId, i, j, value) => {
+    const game = games.find(game => game.id === gameId);
+    if (!game) return;
+    game.map.map[i][j] = value;
+    socket.broadcast.to(gameId).emit('updateMap', i, j, value);
+  });
+  socket.on('updateUser', (gameId, updatedUser) => {
+    console.log('user updated.', Math.random());
+    const game = games.find(game => game.id === gameId);
+    if (!game) return;
+    const user = game.users.find(user => user.uuid === updatedUser.uuid);
+    if (!user) return;
+    Object.assign(user, updatedUser);
+    socket.broadcast.to(gameId).emit('updateUser', updatedUser);
+  });
+  setInterval(() => {
+    const gameIds = games.map(game => game.id);
+    io.to(ROOM_LOBBY).emit('listGames', gameIds);
+  }, 1000);
+});
